53672deb504d37fa0c0a0b101603273aef12db43
aanchal-fatwani <fatwaniaanchal@gmail.com>
2023-05-28 20:02:29 +0530

    Added handling for webpack mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,23 @@
-const path = require('path')
+const path = require("path");
 
-module.exports = {
-    entry: ['./src/index.js', './src/home.js'],
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devtool: "inline-source-map",
-    mode: "development",
-    devServer: {
-        static: "./dist"
-    }
+const isProduction = process.env.NODE_ENV === "production";
+
+let config = {
+  entry: ["./src/index.js", "./src/home.js"],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  mode: "development",
+  devServer: {
+    static: "./dist",
+  },
+};
+
+if (isProduction) {
+  config.mode = "production";
+  config.devtool = "inline-source-map"; // or any other relevant to prod
 }
+
+module.exports = config;
