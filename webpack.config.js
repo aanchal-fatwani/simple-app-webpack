bc70ee211aa421e884d017cbaa8b5e15e6648c06
Aanchal <63228776+aanchal-fatwani@users.noreply.github.com>
2023-07-07 22:36:04 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
+// const CopyPlugin = require("copy-webpack-plugin");
 // const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const HelloWorldPlugin = require("./hw-plugin/index");
 
